de4042af5b1b0e66264c350b53b36625ca313488
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-04-08 17:22:45 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,11 @@
 def display_board
   puts "A Tic Tac Toe Board"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  
 end
 display_board
 
